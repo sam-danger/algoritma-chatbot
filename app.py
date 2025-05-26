4f69c690496a7db7a5c39db32f15c033372ec443
sam-danger <redalper58@gmail.com>
2025-05-26 21:46:43 +0300

    Update app.py
    from flask import Flask, request, jsonify
    import chatbot
    
    app = Flask(__name__)
    
    @app.route("/")
    def home():
        return "Algoritma Chatbot Hazır!"
    
    @app.route("/chatbot", methods=["POST"])
    def bot_cevap():
        veri = request.get_json()
        girdi = veri.get("mesaj", "")
        cevap = chatbot.cevapla(girdi)
        return jsonify({"cevap": cevap})
    
    if __name__ == "__main__":
        app.run(host="0.0.0.0", port=8000)

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 
+from flask import Flask, request, jsonify
+import chatbot
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Algoritma Chatbot Hazır!"
+
+@app.route("/chatbot", methods=["POST"])
+def bot_cevap():
+    veri = request.get_json()
+    girdi = veri.get("mesaj", "")
+    cevap = chatbot.cevapla(girdi)
+    return jsonify({"cevap": cevap})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
